9c72c9d619c048d2b273b1ab2200640fb9ac181f
Jonatán Urquiza <jonatan.urquiza@pedidosya.com>
2025-04-04 23:40:40 -0300

    first commit

---
+++ Sources/FeatureModule/FeatureView.swift
@@ -0,0 +1,18 @@
+//
+//  FeatureView.swift
+//
+//
+//  Created by Jonatán Ezequiel Urquiza Martinez on 04/04/2025.
+//
+
+import SwiftUI
+
+struct FeatureView: View {
+    var body: some View {
+        Text("Hello, Swifteando!")
+    }
+}
+
+#Preview {
+    FeatureView()
+}
